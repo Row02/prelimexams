f7db3269cda7c2232b06120202e7b7aaf4712205
Row02 <143058106+Row02@users.noreply.github.com>
2024-02-15 12:19:04 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,23 @@ export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title></title>
       </Head>
       <section className={utilStyles.headingMd}>
 
-    10 Things That Require Zero Talent</p>
-    <ul>
-    
-    <li>1. Being On Time</li>
-    <li>2. Making An Effort</li>
-    <li>3. Being High Energy</li>
-    <li>4. Having A Positive Attitude</li>
-    <li>5. Being Passionate</li>
-    <li>6. Using Good Body Language</li>
-    <li>7. Being Coachable</li>
-    <li>8. Doing A Little Extra</li>
-    <li>9. Being Prepared</li>
-    <li>10. Having A Strong Work Ethic</li>
-    
-    </ul>
+    <p>10 Things That Require Zero Talent </p>
+<ul>
+        <li>1. Being On Time </li>
+        <li>2. Making An Effort </li>
+        <li>3. Being High Energy</li>
+        <li>4. Having A Positive Attitude</li>
+        <li>5. Being Passionate</li>
+        <li>6. Using Good Body Language</li>
+        <li>7. Being Coachable</li>
+        <li>8. Doing A Little Extra</li>
+        <li>9. Being Prepared</li>
+        <li>10. Having A Strong Work Ethic</li>
+  </ul>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
